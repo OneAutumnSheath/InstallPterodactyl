922785187d0d94fdb3c77c85c5dbabc2e5c65fcf
OneAutumnSheath <96901993+OneAutumnSheath@users.noreply.github.com>
2022-01-23 15:40:16 +0100

    Update installpterodactyl.sh

--- installpterodactyl.sh
+++ installpterodactyl.sh
@@ -1,6 +1,10 @@
-RED='\003[1;31m'
-GREEN='\003[1;32m'
-YELLOW='\003[1;33m'
-BLUE='\003[1;36m'
-NC='\003[0m'
+RED='\033[1;31m'
+ROYAL='\033[1;34m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;36m'
+NC='\033[0m'
 echo -e "${BLUE}Pterodactyl Install Script ${GREEN}by ${RED}OneAutumnSheath${NC}\n"
+echo -e "${BLUE}Twitter: @scorpiondevde"
+
+echo -e "${ROYAL}Please choose between the Panel or Wings Install: "
